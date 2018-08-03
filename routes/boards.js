3b7d79eba9751211138a39c3c3c707f1e1ff53f0
金正朋也 <mhtnim1023@gmail.com>
2018-08-04 00:48:05 +0900

    message投稿機能実装

--- routes/boards.js
+++ routes/boards.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var connection = require('../mysqlConnection');
 
 router.get('/:board_id', function(req, res, next){
@@ -10,6 +11,16 @@ router.get('/:board_id', function(req, res, next){
     title: board[0].title,
     board: board[0]
     });
+  });
+});
+
+router.post('/:board_id', function(req, res, next){
+  var message = req.body.message;
+  var boardId = req.params.board_id;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'insert into messages (message, board_id, created_at) values("'+ message +'", '+' "'+ boardId +'", '+'"'+ createdAt +'")';
+  connection.query(query, function(err, rows){
+    res.redirect('/boards/' + boardId);
   });
 });
 
